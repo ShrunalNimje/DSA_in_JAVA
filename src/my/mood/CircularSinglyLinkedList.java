ce3208d756a7d8b32aabb9f949bad851e0e8b949
Shrunal Nimje <shrunalnimje@gmail.com>
2024-04-08 10:27:00 +0530

    printing elements of CircularSinglyLinkedList

--- src/my/mood/CircularSinglyLinkedList.java
+++ src/my/mood/CircularSinglyLinkedList.java
@@ -31,7 +31,47 @@ public class CircularSinglyLinkedList {
         return length;
     }
 
+    // creating Circular Singly Linked list
+    public void createList(){
+        ListNode first = new ListNode(2);
+        ListNode second = new ListNode(5);
+        ListNode third = new ListNode(12);
+        ListNode fourth = new ListNode(23);
+        ListNode fifth = new ListNode(13);
+        ListNode sixth = new ListNode(65);
+
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
+        fifth.next = sixth;
+        sixth.next = first;
+
+        last = sixth;
+    }
+
+    public void displayList(){
+        if (last == null){
+            System.out.println("List is Empty!");
+            return;
+        }
+
+        ListNode first = last.next;
+
+        while (first != last){
+            System.out.print(first.data + " ");
+            first = first.next;
+            length++;
+        }
+
+        System.out.println(first.data);
+        length++;
+        System.out.println(getLength());
+    }
+
     public static void main(String[] args) {
         CircularSinglyLinkedList circularSinglyLinkedList = new CircularSinglyLinkedList();
+        circularSinglyLinkedList.createList();
+        circularSinglyLinkedList.displayList();
     }
 }
