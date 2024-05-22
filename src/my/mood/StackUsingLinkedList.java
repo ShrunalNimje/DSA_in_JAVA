19ddbcf06333b234f888bda20a97b89553d747a9
Shrunal Nimje <shrunalnimje@gmail.com>
2024-05-22 12:03:04 +0530

    Finding next first greater element & checking valid parenthesis

--- src/my/mood/StackUsingLinkedList.java
+++ src/my/mood/StackUsingLinkedList.java
@@ -1,5 +1,6 @@
 package my.mood;
 
+import java.util.Arrays;
 import java.util.EmptyStackException;
 
 public class StackUsingLinkedList {
@@ -63,6 +64,61 @@ public class StackUsingLinkedList {
         return top.data;
     }
 
+    // Finding first next greater element to right direction in an element of an array
+    public static int [] nextGreaterElement(int [] arr){
+        int [] result = new int[arr.length];
+        StackUsingLinkedList stackUsingLinkedList = new StackUsingLinkedList();
+
+        for (int i = arr.length-1; i >= 0; i--){
+            if (!stackUsingLinkedList.isEmpty()){
+                while (!stackUsingLinkedList.isEmpty() && stackUsingLinkedList.peek() <= arr[i]){
+                    stackUsingLinkedList.pop();
+                }
+            }
+
+            if (stackUsingLinkedList.isEmpty()){
+                result[i] = -1;
+            }
+            else {
+                result[i] = stackUsingLinkedList.peek();
+            }
+            stackUsingLinkedList.push(arr[i]);
+
+        }
+        return result;
+    }
+
+    // Checking valid parenthesis
+    public static boolean isValid(String s) {
+        StackUsingLinkedList stackUsingLinkedList = new StackUsingLinkedList();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '['){
+                stackUsingLinkedList.push(c);
+            }
+
+            else {
+                if (stackUsingLinkedList.isEmpty()){
+                    return false;
+                }
+
+                else {
+                    int top = stackUsingLinkedList.peek();
+                    if ((top == '[' && c == ']') ||
+                            (top == '(' && c == ')') ||
+                            (top == '{' && c == '}')){
+                        stackUsingLinkedList.pop();
+                    }
+
+                    else {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stackUsingLinkedList.isEmpty();
+    }
+
     public static void main(String[] args) {
         StackUsingLinkedList stack = new StackUsingLinkedList();
         // Push operation for deletion data
@@ -74,6 +130,19 @@ public class StackUsingLinkedList {
 
         // Peek operation for top data
         System.out.println("Peek element : " + stack.peek());
+
+        // Finding first next greater element to right direction in an element of an array
+        int [] array = {8,4,6,7,1,9};
+        System.out.println("Array : "+ Arrays.toString(array));
+        System.out.println("Next greater element : " + Arrays.toString(nextGreaterElement(array)));
+
+        // Checking Valid parenthesis
+        String s = "[({)]";
+        String s1 = "[(})]";
+        String s2 = "[({})]";
+        System.out.println("S : "+isValid(s));
+        System.out.println("S1 : "+isValid(s1));
+        System.out.println("S2 : "+isValid(s2));
 
     }
 }
