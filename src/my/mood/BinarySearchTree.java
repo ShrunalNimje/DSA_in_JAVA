97fbe2ec900d644dc1d4f0662e492fcec14194c2
Shrunal Nimje <shrunalnimje@gmail.com>
2024-05-30 20:59:22 +0530

    Insertion of node in BinarySearchTree

---
+++ src/my/mood/BinarySearchTree.java
@@ -0,0 +1,63 @@
+package my.mood;
+
+public class BinarySearchTree {
+    public TreeNode root;
+
+    public static class TreeNode{
+        public int data;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode(int data){
+            this.data = data;
+        }
+    }
+
+    // Insert in Binary Search Tree
+    public void insert(int value){
+        root = insert(root, value);
+    }
+
+    public TreeNode insert(TreeNode root, int value){
+        if (root == null){
+            root = new TreeNode(value);
+            return root;
+        }
+
+        if (value < root.data){
+            root.left = insert(root.left, value);
+        }
+        else {
+            root.right = insert(root.right, value);
+        }
+        return root;
+    }
+
+    // printing node using in-order traversal
+    public void print(){
+        print(root);
+    }
+
+    public void print(TreeNode root){
+        if (root == null){
+            return;
+        }
+
+        print(root.left);
+        System.out.print(root.data+ " ");
+        print(root.right);
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+
+        // Insert in Binary Search Tree
+        binarySearchTree.insert(6);
+        binarySearchTree.insert(4);
+        binarySearchTree.insert(5);
+        binarySearchTree.insert(8);
+        binarySearchTree.insert(7);
+        binarySearchTree.print();
+
+    }
+}
