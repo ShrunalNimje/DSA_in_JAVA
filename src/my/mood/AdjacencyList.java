b11d883a5fa678a873d9deb67645c4b319348362
Shrunal Nimje <shrunalnimje@gmail.com>
2024-06-20 20:05:37 +0530

    Implementation of BFS

--- src/my/mood/AdjacencyList.java
+++ src/my/mood/AdjacencyList.java
@@ -1,6 +1,7 @@
 package my.mood;
 
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class AdjacencyList {
 
@@ -38,13 +39,37 @@ public class AdjacencyList {
         }
         return stringBuilder.toString();
     }
+
+    // Implementation of Breadth First Search ( BFS )
+    public void BFS(int source){
+        Queue<Integer> queue = new LinkedList<>();
+        boolean [] isCorrect = new boolean[vertices];
+        isCorrect[source] = true;
+
+        queue.offer(source);
+        while (!queue.isEmpty()) {
+            int a = queue.poll();
+            System.out.print(a + " ");
+
+            for (int element : adjacencyList[a]){
+                if (!isCorrect[element]){
+                    isCorrect[element] = true;
+                    queue.offer(element);
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        AdjacencyList adjacencyList = new AdjacencyList(4);
+        AdjacencyList adjacencyList = new AdjacencyList(5);
         adjacencyList.addNode(0, 1);
         adjacencyList.addNode(1, 2);
         adjacencyList.addNode(2, 3);
         adjacencyList.addNode(3, 0);
-
+        adjacencyList.addNode(2,4);
         System.out.println(adjacencyList);
+
+        // Implementation of BFS
+        adjacencyList.BFS(0);
     }
 }
