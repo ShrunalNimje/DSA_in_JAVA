44d3d4c665b0239d7613571ce4ddd8dc585268a3
Shrunal Nimje <shrunalnimje@gmail.com>
2024-06-25 21:22:34 +0530

    insertion of words in Trie

--- src/my/mood/Trie.java
+++ src/my/mood/Trie.java
@@ -19,7 +19,39 @@ public class Trie {
         }
     }
 
+    // Insertion of Words in Trie
+    public void insert(String word){
+        if (word.isEmpty()){
+            throw new IllegalArgumentException("Invalid input or word!");
+        }
+
+        word = word.toLowerCase();
+        TrieNode current = root;
+
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            int index = c - 'a';
+
+            if (current.children[index] == null){
+                TrieNode newNode = new TrieNode();
+                current.children[index] = newNode;
+                current = newNode;
+            }
+            else {
+                current = current.children[index];
+            }
+        }
+
+        current.isWord = true;
+    }
+
     public static void main(String[] args) {
         Trie trie = new Trie();
+        trie.insert("ball");
+        trie.insert("basket");
+        trie.insert("catch");
+        trie.insert("camera");
+        trie.insert("crunch");
+        System.out.println("words inserted to trie successfully!");
     }
 }
